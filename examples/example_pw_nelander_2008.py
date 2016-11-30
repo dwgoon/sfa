7d9e00662c7c8e53c90406c78f3da0ddfd4ef953
dwlee <mustarddream@gmail.com>
2016-11-30 18:03:52 +0900

    Add some examples and modify data files

---
+++ examples/example_pw_nelander_2008.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import copy
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+import sfa
+import sfa.vis
+from sfa import calc_accuracy
+from sfa import AlgorithmSet
+from sfa import DataSet
+
+
+if __name__ == "__main__":
+
+    # Create containers for algorithm and data.
+    algs = AlgorithmSet()
+    ds = DataSet()
+
+    # Load an algorithm and a data.
+    algs.create('SP')
+    alg = algs['SP']
+    
+    ds.create("NELANDER_2008")
+    data = ds["NELANDER_2008"]
+
+    results = {}
+    data.A[data.n2i["p-ERK"], data.n2i["p-MEK"]]
+    alg.data = data
+    alg.params.initialize()
+    alg.params.use_rel_change = True
+    alg.initialize()
+    alg.compute_batch()
+    acc, cons = calc_accuracy(alg.result.df_sim,
+                              data.df_exp,
+                              get_cons=True)
+    
+    print ("Accuracy: ", acc)
+#    brt = sfa.vis.BatchResultTable(data, cons)
+#    brt.column_label_fontsize = 4
+#    brt.ax.tick_params(axis='x', which='both', pad=0.01)
+#    
+#    brt.row_label_fontsize = 4
+#    brt.ax.tick_params(axis='y', which='both', pad=0.01)
+#
+#    plt.subplots_adjust(left=0.1, bottom=0.05, right=0.9, top=0.8,
+#                        wspace=0.1, hspace=0.1)
+#
+#    brt.fig.set_size_inches(2, 3)
+#    brt.fig.savefig("%s_%s.png"%(alg.abbr, data.abbr), dpi=300)
+    
+#    
+#    ptbs = ['EGF', 'IGF1R','PI3K','mTOR','PKCdelta','p-MEK','EGFR']
+#    vals = [1, -1, -1, -1, -1, -1, -1]
+#    paths = []
+#    for i, src in enumerate(ptbs):
+#        x, p = alg.wire([src], [vals[i]], get_path=True)
+#        paths.extend(p)
+#    
+#    paths = sorted(paths, key=lambda p: p[-1])
+#    
+
+
+#    x, paths = alg.wire(['EGF', 'p-MEK', 'EGFR'], [1, -1, -1], get_path=True)
+#   
+#    def get_link_str(x, y):
+#        sign = data.dg.edge[x][y]['sign']
+#        if sign>0:
+#            return '->'
+#        else:
+#            return '-|'
+#    
+#    
+#    for p in paths:
+#        str_path = str(p[0])
+#        if p[-1] != 'p-RAF':
+#            continue
+#            
+#        for i in range(len(p)-1):
+#            src = p[i]
+#            tgt = p[i+1]            
+#            str_link = get_link_str(src, tgt)
+#            str_path += " %s %s"%(str_link, tgt)
+#    
+#        if p[0] == 'EGF':
+#            ba = +1
+#        else:
+#            ba = -1
+#            
+#        F = alg.wire_single_path(data.dg, ba, p)
+#        print (F, str_path)
+
+# end of main
---
+++ examples/sp_vs_ncps_molinelli_2013.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import copy
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import sfa
+import sfa.vis
+from sfa import calc_accuracy
+from sfa import AlgorithmSet
+from sfa import DataSet
+
+plt.ioff()
+
+if __name__ == "__main__":
+
+    # Create containers for algorithm and data.
+    algs = AlgorithmSet()
+    ds = DataSet()
+
+    # Load an algorithm and a data.
+    #algs.create(["CPS", "SP"])
+    algs.create()
+
+    algs["APS"] = algs["PW"]
+    del algs["PW"]
+    algs["APS"].abbr = "APS"
+    
+    # Normalized CPS
+    algs["NCPS"] = copy.deepcopy(algs["CPS"])
+    algs["NCPS"].abbr = "NCPS"
+    algs["NCPS"].params.apply_weight_norm = True
+
+    algs["NAPS"] = copy.deepcopy(algs["APS"])
+    algs["NAPS"].abbr = "NAPS"
+    algs["NAPS"].params.initialize()
+    algs["NAPS"].params.apply_weight_norm = True
+
+    ds.create("MOLINELLI_2013")
+    data = ds["MOLINELLI_2013"]
+    
+    #data.df_ba.drop(data.df_ba.columns[:17], axis=1, inplace=True)
+    #data.df_exp.drop(data.df_exp.columns[17:], axis=1, inplace=True)
+    #data.df_exp = data.df_exp[['cellprol']]
+        
+    dfs = {}
+    results = {}
+    for abbr, alg in algs.items():        
+        alg.params.use_rel_change = True
+        #alg.params.exsol_forbidden = True
+        alg.data = data
+        alg.initialize()
+        
+        #W = sfa.normalize(data.A, norm_in=False)
+        #alg.W = W
+        
+        alg.compute_batch()
+        #df_sim = alg.result.df_sim.ix[:, :17]
+        #df_sim = alg.result.df_sim[['cellprol']]
+        df_sim = alg.result.df_sim
+
+        acc, cons = calc_accuracy(df_sim,
+                                  data.df_exp,
+                                  get_cons=True)
+
+        brt = sfa.vis.BatchResultTable(data, cons)
+        brt.fig.set_size_inches(4, 6)
+        brt.fig.savefig("%s_%s.png"%(alg.abbr, data.abbr), dpi=300)
+        #brt.fig.savefig("output_%s_%s.png"%(alg.abbr, data.abbr), dpi=300)
+    
+        results[abbr] = acc
+        print ("%s: %.3f"%(abbr, acc))
+        dfs[abbr] = df_sim
+    # end of for
+        
+
+#    df = pd.DataFrame.from_dict(results, orient='index')
+#    df.columns = ['MOLINELLI_2013']
+#    
+#    df_sort = df.sort_values(by='MOLINELLI_2013')
+#    print(df_sort)
+    plt.close("all")
+    
+    #df_diff = (dfs["SP"] == dfs["NCPS"])
+
+#    ht = sfa.vis.HeatmapTable()
+#    ht.table_fontsize = 10
+#    ht.fig.set_size_inches(6, 8)
+#    plt.subplots_adjust(left=0.4, bottom=0.05, right=0.9, top=0.9,
+#                        wspace=0.4, hspace=0.2)
+#
+#    ht.fig.savefig("SP_vs_NCPS.png", dpi=300)    
+
+# end of main
