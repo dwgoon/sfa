2acd4dd6e14159de6e4abe58cd43489dc29b8f6e
Daewon Lee <daewon4you@gmail.com>
2017-11-18 20:54:06 +0900

    Change calc_effects() to calc_influence()

--- sfa/analysis/__init__.py
+++ sfa/analysis/__init__.py
@@ -2,7 +2,7 @@
 
 
 from .perturb import analyze_perturb
-from .effects import calc_effects
+from .influence import calc_influence
 
 from .random.weight import RandomWeightBatchSimulator
 from .random.structure import RandomStructureBatchSimulator
