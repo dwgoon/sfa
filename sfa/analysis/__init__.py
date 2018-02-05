197f1340b34c07a6d81f7dae829557c0ecfb7318
Daewon Lee <daewon4you@gmail.com>
2018-02-05 16:26:13 +0900

    Revise documentation of calc_influence

--- sfa/analysis/__init__.py
+++ sfa/analysis/__init__.py
@@ -2,7 +2,6 @@
 
 
 from .perturb import analyze_perturb
-from .influence import calc_influence
 
 from .random.weight import RandomWeightBatchSimulator
 from .random.structure import RandomStructureBatchSimulator
---
+++ sfa/control/__init__.py
@@ -0,0 +1,2 @@
+
+from .influence import calc_influence
